45c94fb38771d8229ef13e12e84cd85437991dec
dasBente <dasbente@gmail.com>
2026-03-05 14:52:34 +0100

    undo dendritic modules
    For all intents and purposes, this approach
    is easier as a mere build tool.

--- templates/godot/flake.nix
+++ templates/godot/flake.nix
@@ -1,12 +1,30 @@
 {
-  description = "A very basic flake";
+  description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        self',
+        pkgs,
+        ...
+      }: {
+        packages.godot-wrapped = pkgs.writeShellScriptBin "godot" ''
+          exec ${pkgs.godot-mono}/bin/godot-mono -e . "$@" > /dev/null
+        '';
+
+        devShells.default = pkgs.mkShell {
+          packages = [self'.packages.godot-wrapped];
+          nativeBuildInputs = with pkgs; [
+            mono
+            godot-mono
+          ];
+        };
+      };
+    };
 }
--- templates/godot/modules/godot-wrapped.nix
+++
@@ -1,7 +0,0 @@
-{
-  perSystem = {pkgs, ...}: {
-    packages.godot-wrapped = pkgs.writeShellScriptBin "godot" ''
-      exec ${pkgs.godot-mono}/bin/godot-mono -e . "$@" > /dev/null
-    '';
-  };
-}
