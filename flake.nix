7ed19dee79636b60cb9c1e61e0adbfbffc9cea15
dasBente <dasbente@gmail.com>
2026-03-05 15:31:24 +0100

    refactor to use flake-parts
    Useful when we add utils down the line

--- flake.nix
+++ flake.nix
@@ -1,15 +1,29 @@
 {
-  description = "My own templates";
+  description = "Description for the project";
 
-  outputs = {self}: let
-    templates = builtins.mapAttrs (name: _: {
-      path = ./templates/${name};
-      description = let
-        flake = import ./templates/${name}/flake.nix;
-      in
-        flake.description or "${name} template";
-    }) (builtins.readDir ./templates);
-  in {
-    inherit templates;
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs = let
+    # list of all template directories
+    template-dirs = builtins.readDir ./templates;
+
+    # list of flake-style template outputs
+    templates =
+      builtins.mapAttrs (name: _: {
+        path = ./templates/${name};
+        description = let
+          flake = import ./templates/${name}/flake.nix;
+        in
+          flake.description or "template: ${name}";
+      })
+      template-dirs;
+  in
+    inputs @ {flake-parts, ...}:
+      flake-parts.lib.mkFlake {inherit inputs;} {
+        systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+        flake = {inherit templates;};
+      };
 }
